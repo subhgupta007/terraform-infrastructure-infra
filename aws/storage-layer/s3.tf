31d89b1a010a8a3bd8def094c28a796ec02d54ba
Gupta <subhgupta@deloitte.com>
2024-08-27 11:57:44 +0530

    adding  s3 in storage layer

--- aws/storage-layer/s3.tf
+++ aws/storage-layer/s3.tf
@@ -1,12 +1,12 @@
 module "s3_shakti_store {
   source                               = "git@github.com:subhgupta007/cmn-terraform-module-infrastructure.git//aws/s3-bucket?ref=master"
-  bucket_name                          = var.file_import_s3.bucket_name
-  versioning                           = var.file_import_s3.versioning
+  bucket_name                          = var.s3_shakti.bucket_name
+  versioning                           = var.s3_shakti.versioning
   enable_s3_bucket_public_access_block = true
   bucket_block_public_acls             = true
   bucket_block_public_policy           = true
   bucket_ignore_public_acls            = true
   bucket_restrict_public_buckets       = true
   logging                              = true
-  target_bucket                        = var.file_import_s3.target_bucket
+  target_bucket                        = var.s3_shakti.target_bucket
 }
